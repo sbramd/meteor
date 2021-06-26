e4113d6758b49966756e340f3bb3b0d4bf008b6d
sabbir0179 <sahmed@anynines.com>
2021-06-26 20:47:43 +0200

    Reconfigure stylelint rules and packages

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -14,14 +14,14 @@ export const AppLogo = styled.img`
 `;
 
 export const AppHeader = styled.header`
-	background-color: #282c34;
-	min-height: 100vh;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-	font-size: calc(10px + 2vmin);
+	min-height: 100vh;
 	color: white;
+	font-size: calc(10px + 2vmin);
+	background-color: #282c34;
 `;
 
 export const AppLink = styled.a`
